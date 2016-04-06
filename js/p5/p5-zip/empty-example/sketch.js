313532b320d31887d1523f63978a39337a15876a
Brian Lopez <brian.lopez@alphapublicschools.org>
2016-04-06 14:48:48 +0000

    Blog

---
+++ js/p5/p5-zip/empty-example/sketch.js
@@ -0,0 +1,13 @@
+var img;
+function preload(){
+    img = loadImage("./aggreseiveconductorig1.png");
+}
+
+function setup() {
+    createCanvas(400, 400);
+    image(img, 0 , 0);
+}
+
+function draw() {
+    
+}
